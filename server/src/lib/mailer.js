2bcf724c21468ccffeecc1c14e24282f3596e817
Priyanshi Jain <jpriyanshi121@gmail.com>
2026-09-23 09:57:53 +0530

    Switch to Gmail OAuth2 for password reset emails

--- server/src/lib/mailer.js
+++ server/src/lib/mailer.js
@@ -1,14 +1,37 @@
 const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
+const OAuth2 = google.auth.OAuth2;
+
+const oauth2Client = new OAuth2(
+  process.env.GMAIL_CLIENT_ID,
+  process.env.GMAIL_CLIENT_SECRET,
+  "https://developers.google.com/oauthplayground"
+);
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.GMAIL_REFRESH_TOKEN,
 });
 
+async function getTransporter() {
+  const accessToken = await oauth2Client.getAccessToken();
+
+  return nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.EMAIL_USER,
+      clientId: process.env.GMAIL_CLIENT_ID,
+      clientSecret: process.env.GMAIL_CLIENT_SECRET,
+      refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+      accessToken: accessToken.token,
+    },
+  });
+}
+
 async function sendResetEmail(to, resetLink) {
+  const transporter = await getTransporter();
+
   await transporter.sendMail({
     from: `"AttendEasy" <${process.env.EMAIL_USER}>`,
     to,
